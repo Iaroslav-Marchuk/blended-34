0fa64dabe519e119d7416c8a53339bc8b9b802d0
Yurii Pasternak <yuriipasternak@icloud.com>
2025-01-11 14:43:46 +0100

    blended-34

--- js/lesson1.js
+++ js/lesson1.js
@@ -20,13 +20,46 @@
 // Виведіть в alert відповідне повідомлення, наприклад:
 // "10 входить в першу чверть"
 
-const min = Math.floor(Math.random() * (59 - 0) + 0);
-if (min <= 15) {
-  alert(`${min} входить в першу чверть`);
-} else if (min <= 30) {
-  alert(`${min} входить в другу чверть`);
-} else if (min <= 45) {
-  alert(`${min} входить в третю чверть`);
-} else {
-  alert(`${min} входить в четверту чверть`);
+//const min = Math.floor(Math.random() * (59 - 0) + 0);
+//if (min <= 15) {
+//   alert(`${min} входить в першу чверть`);
+// } else if (min <= 30) {
+//   alert(`${min} входить в другу чверть`);
+// } else if (min <= 45) {
+//   alert(`${min} входить в третю чверть`);
+// } else {
+//   alert(`${min} входить в четверту чверть`);
+// }
+
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по зменьшенню
+//Виведіть в консоль суму усіх парних чисел
+// * перепиши код на функцію getNumbers(min, max)
+
+// const min = 0;
+// const max = 20;
+// let sum = 0;
+// for (let i = max; i >= min; i--){
+//     console.log(i)
+//     if (i % 2 === 0) {
+//         sum += i;
+        
+//     }
+// }
+
+// console.log(sum)
+
+function getNumbers(min, max) {
+    let sum = 0;
+    for (let i = max; i >= min; i--){
+        console.log(i)
+        if (i % 2 === 0) {
+            sum += i;
+            
+        }
+    }
+    return sum
 }
+
+console.log(getNumbers(0, 20))
