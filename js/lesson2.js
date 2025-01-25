7858eea70f464627a9e6ba2a36d448b5c414ff14
Inna Kmit <inna.km7@gmail.com>
2025-01-25 13:29:09 +0000

    task3

--- js/lesson2.js
+++ js/lesson2.js
@@ -7,54 +7,70 @@
 // <номер елемента> - <значення елемента>
 // Нумерація має починатись з 1
 
-const styles = ["jazz", "blues"];
+// const styles = ["jazz", "blues"];
 
-styles.push("rock-n-roll");
+// styles.push("rock-n-roll");
 
-// styles[1] = "classic";
+// // styles[1] = "classic";
 
-const index = styles.indexOf("blues");
+// const index = styles.indexOf("blues");
 
-if (index !== -1) {
-  styles[index] = "classic";
-}
+// if (index !== -1) {
+//   styles[index] = "classic";
+// }
 
-function logItems(array) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    console.log(`${i + 1} - ${element}`);
-  }
-}
+// function logItems(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(`${i + 1} - ${element}`);
+//   }
+// }
 
-console.log(logItems(styles));
-
-
-// 2. Напиши функцію checkLogin(array), яка перебирає масив логінів і перевіряє
-// чи є ім'я введене в prompt у цьому масиві і у разі,
-// якщо є - виводить повідомлення в alert "Welcome, <name>!"
-// в іншому випадку - "User not found"
-
-const logins = ["Peter", "John", "Igor", "Sasha"];
+// console.log(logItems(styles));
 
 
-function checkLogin(array) {
-    const login = prompt("Enter your login");
+// // 2. Напиши функцію checkLogin(array), яка перебирає масив логінів і перевіряє
+// // чи є ім'я введене в prompt у цьому масиві і у разі,
+// // якщо є - виводить повідомлення в alert "Welcome, <name>!"
+// // в іншому випадку - "User not found"
 
-    // for (const name of array) {
-    //     if (name === login) {
-    //         return `Welcome, ${name}!`
-    //     }
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+
+
+// function checkLogin(array) {
+//     const login = prompt("Enter your login");
+
+//     // for (const name of array) {
+//     //     if (name === login) {
+//     //         return `Welcome, ${name}!`
+//     //     }
        
-    // }
-    // return "User not found";
+//     // }
+//     // return "User not found";
 
-    return array.includes(login) ? `Welcome, ${login}!`: "User not found"
+//     return array.includes(login) ? `Welcome, ${login}!`: "User not found"
       
 
+// }
+
+// alert(checkLogin(logins));
+
+
+// Напишіть функцію, яка складатиме сусідні числа і пушитиме їх в новий масив
+
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+function sum(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length-1; i++){
+    const currentEl = array[i];
+    const nextEl = array[i + 1];
+
+    newArray.push(currentEl + nextEl);
+
+  }
+  return newArray;
 }
-
-alert(checkLogin(logins));
-
-
+console.log(sum(someArr));
 
 
