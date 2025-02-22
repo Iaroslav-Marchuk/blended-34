91b1abbc3ee36ae8c401e58d98e97d3790672405
iaroslav_marchuk <jarik.13a@gmail.com>
2025-02-22 13:43:26 +0000

    add task 4.3

--- js/lesson4.js
+++ js/lesson4.js
@@ -53,33 +53,108 @@
 // Спочатку на екрані з'являється якась фігура рандомного коліру в рандомному місті
 // Натискаючі на фігуру, вона змінює свою форму, колір, місце розташування
 
-const forms = [
-  "width: 100px; height: 100px; border-width: 1px;",
-  "width: 100px; height: 100px; border-radius: 50%;",
-  "width: 150px; height: 100px; border-width: 1px;",
-  "width: 200px; height: 100px; border-radius: 100px / 50px; ",
-  "width: 150px; height: 100px; transform: skew(20deg)",
+// const forms = [
+//   "width: 100px; height: 100px; border-width: 1px;",
+//   "width: 100px; height: 100px; border-radius: 50%;",
+//   "width: 150px; height: 100px; border-width: 1px;",
+//   "width: 200px; height: 100px; border-radius: 100px / 50px; ",
+//   "width: 150px; height: 100px; transform: skew(20deg)",
+// ];
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function randomither(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// const newElement = document.createElement("div");
+// randomShape();
+
+// document.body.append(newElement);
+
+// newElement.addEventListener("click", randomShape);
+
+// function randomShape() {
+//   newElement.style.cssText = forms[randomither(forms.length - 1)];
+//   newElement.style.backgroundColor = getRandomHexColor();
+//   newElement.style.position = "absolute";
+//   newElement.style.top = `${randomither(95)}%`;
+//   newElement.style.left = `${randomither(95)}%`;
+// }
+
+
+
+
+
+
+
+// 3. Створити розмітку на основі масива даних, де у кожного елемента списку
+// буде вказано ім'я, кількість лайків і перелічені теги
+// в index.html додайте список ul.stats, в який буде рендеритись цей список
+// *властивість gender використай для додавання відповідного класу елементу списка
+
+
+const tweets = [
+  { id: "000", name: "Alex", gender: "male", likes: 5, tags: ["js", "nodejs"] },
+  {
+    id: "001",
+    name: "Kate",
+    gender: "female",
+    likes: 2,
+    tags: ["html", "css"],
+  },
+  {
+    id: "002",
+    name: "Maria",
+    gender: "female",
+    likes: 17,
+    tags: ["html", "js", "nodejs"],
+  },
+{
+    id: "003",
+    name: "Borys",
+    gender: "male",
+    likes: 8,
+    tags: ["css", "react"],
+  },
+  {
+    id: "004",
+    name: "Jhon",
+    gender: "male",
+    likes: 10,
+    tags: ["js", "nodejs", "react"],
+  },
+{
+    id: "005",
+    name: "Anna",
+    gender: "female",
+    likes: 3,
+    tags: ["js", "nodejs", "react"],
+  },
+  {
+    id: "006",
+    name: "Jhon",
+    gender: "male",
+    likes: 0,
+    tags: ["js", "nodejs", "react"],
+  },
 ];
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
-function randomither(max) {
-  return Math.floor(Math.random() * max);
-}
 
-const newElement = document.createElement("div");
-randomShape();
+const stats = document.querySelector(".stats");
+let markup = '';
 
-document.body.append(newElement);
 
-newElement.addEventListener("click", randomShape);
+tweets.forEach(item => {
+  markup += `<li class="stats-item ${item.gender}">
+        <p class="stats-name">${item.name}</p>
+        <p class="stats-likes">${item.likes} likes</p>
+        <p class="stats-tags">${item.tags}</p>
+      </li>`
+})
 
-function randomShape() {
-  newElement.style.cssText = forms[randomither(forms.length - 1)];
-  newElement.style.backgroundColor = getRandomHexColor();
-  newElement.style.position = "absolute";
-  newElement.style.top = `${randomither(95)}%`;
-  newElement.style.left = `${randomither(95)}%`;
-}
+stats.innerHTML = markup;
+
